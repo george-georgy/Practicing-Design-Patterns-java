5c44fa4afdaa830bbff621a3d00a7e51a4a8eef8
George <george_georgy@outlook.com>
2023-05-29 20:06:25 +0300

    commit adapter pattern

---
+++ src/Adapter/EnemyRobotAdapter.java
@@ -0,0 +1,28 @@
+package Adapter;
+
+// Adapter
+public class EnemyRobotAdapter implements EnemyAttacker {
+
+    // reference of adaptee
+    EnemyRobot theRobot;
+
+    public EnemyRobotAdapter(EnemyRobot enemyRobot) {
+        theRobot = enemyRobot;
+    }
+
+    @Override
+    public void fireWeapon() {
+        theRobot.smashWithHands();
+    }
+
+    @Override
+    public void driveForward() {
+        theRobot.walkForward();
+    }
+
+    @Override
+    public void assignDriver(String driverName) {
+
+        theRobot.reachToHuman(driverName);
+    }
+}
